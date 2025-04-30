8cc2039927af1810b3bcce81f9fb09562a74704c
sahil-jindal-22 <sahilj.webdev@gmail.com>
2025-05-01 02:18:42 +0530

    fixed typo in URL

--- main.js
+++ main.js
@@ -571,7 +571,7 @@ const initTracking = {
             "dev.d3tafas16ltk5f.amplifyapp.com"
           );
           link.href = link.href.replace(
-            "www.zara.client.micro1.ai",
+            "www.zara.micro1.ai",
             "feat-zara-dashboard.d1y3udqq47tapp.amplifyapp.com"
           );
         }
